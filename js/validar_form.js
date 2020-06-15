80c6137b4424c802cf3c03c254bf63442b58b116
jerryramos <jerryramos1995@gmail.com>
2020-06-15 02:43:09 -0100

    add file validar form .js

---
+++ js/validar_form.js
@@ -0,0 +1,50 @@
+function validar() {
+  let nome = document.getElementById("nome");
+  let apelido = document.getElementById("apelido");
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  let masc = document.getElementById("masc");
+  let fem = document.getElementById("fem");
+  let outro = document.getElementById("outro");
+  let ilha = document.getElementById("ilha");
+  if (
+    ((nome.value &&
+      apelido.value &&
+      password.value &&
+      ilha.value &&
+      masc.checked) ||
+      fem.checked ||
+      outro.checked) == ""
+  ) {
+    alert("Nao pode conter campos vazios");
+  } else {
+    if (!validateEmail()) {
+      alert("Email invalido!");
+    } else {
+      add_pessoa();
+    }
+  }
+}
+
+function add_pessoa() {
+  let array_pessoa = [];
+
+  let pessoa = {
+    nome: nome.value,
+    apelido: apelido.value,
+  };
+
+  array_pessoa.push(pessoa);
+
+  alert(
+    "Bem Vindo " +
+      (array_pessoa[0].nome + " " + array_pessoa[0].apelido).toUpperCase()
+  );
+}
+
+function validateEmail() {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    return true;
+  }
+  return false;
+}
